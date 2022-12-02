0a9679b29ea0de055521aa90fd0c3856d8b15a48
Кирилл Евгеньевич <123@mail.ru>
2022-12-02 16:44:24 +0300

    finished project

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-a = 5
-print(a)
-
+def line(string):
+   vowels = ['у','е','ы','а','о','э','я','и','ю']
+#   newline = string.lower()
+   total = 0
+   for s in string:
+      if s.lower() in vowels:
+         total += 1
+   return total
+print(line("ПРИВЕТ"))
